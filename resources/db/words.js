9e2472d5dbe764eee57ad0dc6445b5ece208d04c
Vikas Kumar <63574401+vk498959@users.noreply.github.com>
2023-03-18 22:32:23 +0530

    Add files via upload

---
+++ resources/db/words.js
@@ -0,0 +1,1 @@
+var a=[["this is an example 1 for typing you can type this is sample text only to type "],["he is playing cricket and he is a good batsman"],["This car is very good to drive i like this car"]]
