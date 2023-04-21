98d8afd9aab29ea3658e921b73e9e155b8218581
Vikas Kumar <63574401+vk498959@users.noreply.github.com>
2023-04-21 21:35:38 +0530

    Delete resources directory

--- resources/db/words.js
+++
@@ -1,1 +0,0 @@
-var a=[["this is an example 1 for typing you can type this is sample text only to type "],["he is playing cricket and he is a good batsman"],["This car is very good to drive i like this car"]]
