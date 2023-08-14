82f6c33bbc8419d9b52ec70949e33dda35d71e52
Ankit kumar <ak36924648@gmail.com>
2023-08-14 23:07:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const express=require("express");
-const app=express();
-const db=require("./dbfunctions.js");
-//middleware
+const express = require("express");
+const app = express();
+const index=require("./routes/index.js");
+const api=require("./routes/api");
+const valid=require("./middlewares/valid")
+
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.listen(8000,()=>{
+app.use("/",index)
+app.use("/api",api)
+app.set("view engine", "ejs")
+app.use(valid);
+app.listen(8000, () => {
     console.log("server is running on port 8000")
 });
-app.get("/home",(req,res)=>{
-    res.sendfile("public/index.html")
-})
-
-app.post("/api/v1",(req,res)=>{
-    
-    var x=req.body.level;
-    res.json(db.typing(x));
-})
-app.get("/",(req,res)=>{
-    res.json("this is the more info of this api")
-});
-
